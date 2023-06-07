ddcc8b4387e90147356da1932fdfafc87bef403a
Berkay Urun <berkayurun@outlook.com>
2023-06-07 10:57:19 +0300

    fix: remove whitespaces on Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,10 @@ export function Footer() {
   return (
     <footer className="text-center text-neutral-50 bg-gradient-to-t from-sky-500 to-sky-400">
       <p className="flex items-center justify-center gap-2 py-2">
-        Made with <BsFillHeartFill className="text-red-500" /> by{" "}
+        Made with <BsFillHeartFill className="text-red-500" /> by
         <a href="https://www.github.com/brkyurun" className="hover:underline">
           brkyurun
-        </a>{" "}
+        </a>
       </p>
     </footer>
   );
