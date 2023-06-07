21adbd862471633df6842c4925c3d0dcd480214f
Berkay Urun <berkayurun@outlook.com>
2023-06-07 11:32:02 +0300

    refactor: move sample data to utils for clean code

---
+++ src/utils/sampleData.ts
@@ -0,0 +1,52 @@
+import { Cat } from "../types";
+
+export const SAMPLE_DATA: Cat[] = [
+  {
+    id: "9gp",
+    url: "https://cdn2.thecatapi.com/images/9gp.jpg",
+  },
+  {
+    id: "50o",
+    url: "https://cdn2.thecatapi.com/images/50o.jpg",
+  },
+  {
+    id: "9fn",
+    url: "https://cdn2.thecatapi.com/images/9fn.jpg",
+  },
+  {
+    id: "at3",
+    url: "https://cdn2.thecatapi.com/images/at3.jpg",
+  },
+  {
+    id: "av4",
+    url: "https://cdn2.thecatapi.com/images/av4.jpg",
+  },
+  {
+    id: "d46",
+    url: "https://cdn2.thecatapi.com/images/d46.jpg",
+  },
+  {
+    id: "dcn",
+    url: "https://cdn2.thecatapi.com/images/dcn.jpg",
+  },
+  {
+    id: "egr",
+    url: "https://cdn2.thecatapi.com/images/egr.png",
+  },
+  {
+    id: "Z6mrcccZv",
+    url: "https://cdn2.thecatapi.com/images/Z6mrcccZv.jpg",
+  },
+  {
+    id: "INXwfT_cp",
+    url: "https://cdn2.thecatapi.com/images/INXwfT_cp.jpg",
+  },
+  {
+    id: "a96",
+    url: "https://cdn2.thecatapi.com/images/a96.jpg",
+  },
+  {
+    id: "aas",
+    url: "https://cdn2.thecatapi.com/images/aas.jpg",
+  },
+];
